292805d835ba914485f3a522cc7ffd5f7e2d1205
enzobaciocco <94703058+Enzobaciocco@users.noreply.github.com>
2022-08-24 21:19:41 -0400

    Add files via upload

---
+++ tareaenzo1.2.py
@@ -0,0 +1,25 @@
+
+import numpy as np
+import matplotlib.pyplot as plt 
+x = np.array([0.1, 1.0, np.pi/3])
+def f(x):
+    return np.sqrt(x)
+def deriv2(f,x,h): 
+    return (f(x+h)-2*f(x)+f(x-h))/h**2
+df   = (1/2)*x**(-1/2)#primera derivada analitica
+valorreal = 1/4*x**(-3/2) #segunda derivada analitica
+h =  np.geomspace(1, 1e-20,3) # 3 valores para h 
+eps= 2e-52 # tiene que ver con el valor minimo que puede guardar un computador 
+#ahora tengo que solucionar el tema de los distintos tama
+error_an = 0.5*h*np.abs(np.sqrt(x))+2*eps*np.abs(np.sqrt(x))
+valordelaformula = deriv2(f,x,h)
+error_num = np.abs(valorreal-valordelaformula)
+np.argmin(error_num)#donde esta el valor minimo 
+print(error_num)#porque me da un nan
+print("los valores de x son,",x)
+print("los valores de la raiz de x son",f(x))
+print("los valores de h" ,h)
+#np.argmin(error_num)
+print ("el menor error es de ",error_num)
+plt.plot(h,error_num)
+
